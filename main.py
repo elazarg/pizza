15e5b171840f7f4c0bf1087676e5c65083537d8e
elazar <elazarg@gmail.com>
2017-02-21 03:21:05 +0200

    Well, using a table seems to be faster. Lets hope it is also correct

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ R, C, L, H, rows = read_file('small.in')
 
 total_mushrooms = np.cumsum(np.cumsum(rows, axis=0), axis=1)
 TOTAL_SLICES_SIZE = Int('TOTAL_SLICES_SIZE')
-TOTAL = Array('TOTAL', z3.IntSort(), z3.ArraySort(z3.IntSort(), IntSort()))
-INIT = [TOTAL[r][c] == int(total_mushrooms[r, c])
+TOTAL = Function('TOTAL',  z3.IntSort(), z3.IntSort(), IntSort())
+INIT = [TOTAL(r, c) == int(total_mushrooms[r, c])
         for r in range(R)
         for c in range(C)]
-INIT += [TOTAL[r][-1] == 0 for r in range(R)]
-INIT += [TOTAL[-1][c] == 0 for c in range(C)]
+INIT += [TOTAL(r, -1) == 0 for r in range(-1, R)]
+INIT += [TOTAL(-1, c) == 0 for c in range(-1, C)]
 
-SIZES = Array('SIZES', z3.IntSort(), z3.ArraySort(z3.IntSort(), IntSort()))
-INIT += [SIZES[r][c] == r*c
+SIZES = Function('SIZES', z3.IntSort(), z3.IntSort(), IntSort())
+INIT += [SIZES(r, c) == (r+1)*(c+1)
          for r in range(R)
          for c in range(C)]
-INIT += [SIZES[r][-1] == 0 for r in range(R)]
-INIT += [SIZES[-1][c] == 0 for c in range(C)]
+INIT += [SIZES(r, -1) == 0 for r in range(-1, R)]
+INIT += [SIZES(-1, c) == 0 for c in range(-1, C)]
 
 Point = namedtuple('Point', ('c', 'r', ))
 SL = namedtuple('SL', ('src', 'dst', 'i', ))
@@ -42,13 +42,17 @@ SL = namedtuple('SL', ('src', 'dst', 'i', ))
 def slice_size(s):
     sr, sc = s.src.r, s.src.c
     dr, dc = s.dst.r, s.dst.c
-    return SIZES[dr][dc] - SIZES[dr][sc-1] - SIZES[sr-1][dc] + SIZES[sr-1][sc-1]
+    return SIZES(dr, dc) - SIZES(dr, sc-1) - SIZES(sr-1, dc) + SIZES(sr-1, sc-1)
+
+
+# def slice_size(s):
+#     return (s.dst.r - s.src.r + 1) * (s.dst.c - s.src.c + 1)
 
 
 def count_mushrooms(s):
     sr, sc = s.src.r, s.src.c
     dr, dc = s.dst.r, s.dst.c
-    return TOTAL[dr][dc] - TOTAL[dr][sc-1] - TOTAL[sr-1][dc] + TOTAL[sr-1][sc-1]
+    return TOTAL(dr, dc) - TOTAL(dr, sc-1) - TOTAL(sr-1, dc) + TOTAL(sr-1, sc-1)
 
 
 def VAR_SLICE_SIZE(i):
@@ -83,9 +87,8 @@ def create_slices(num):
                  i=i)
               for i in range(num)]
     constraints = [slice_constraints(s) for s in slices]
-    constraints += [cons_nonoverlap(s1, s2) for s1 in slices for s2 in slices if id(s1) < id(s2)]
-    constraints += [TOTAL_SLICES_SIZE == Sum([VAR_SLICE_SIZE(i) for i in range(num)])]
-    constraints += [TOTAL_SLICES_SIZE <= R * C]
+    constraints += [cons_nonoverlap(s1, s2) for s1 in slices for s2 in slices if s1.i < s2.i]
+    constraints += [TOTAL_SLICES_SIZE == Sum([VAR_SLICE_SIZE(s.i) for s in slices])]
     return slices, constraints
 
 
@@ -93,6 +96,7 @@ def optimize(constraints, maximize=True):
     s = Optimize()
     s.add(INIT)
     s.add(constraints)
+    # print(s.sexpr())
     if maximize:
         s.maximize(TOTAL_SLICES_SIZE)
     s.check()
