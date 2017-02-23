1663d7c2f4a2616db123e6d4cb65727cb0b31075
elazar <elazarg@gmail.com>
2017-02-23 22:07:47 +0200

    solve nothing

--- main.py
+++ main.py
@@ -51,15 +51,14 @@ def find_max(xs):
     return m
 
 
-class Constraints:
-    CAPACITY = [Sum([If(has_video[i][j], S[i], 0) for i in range(V)]) <= X
-                for j in range(C)]
+def L(r):
+    return find_max([If(has_video[r.v][j], Int(endpoints[r.e].L_D-endpoints[r.e].L[j]), 0)
+                    for j in range(C) if j in endpoints[r.e].L])
 
-    def L(r):
-        return find_max([If(has_video[r.v][j], endpoints[r.e].L_D-endpoints[r.e].L[j], 0)
-                        for j in range(C) if j in endpoints[r.e].L])
+SERVE = Sum([L(r)*r.n for r in requests]) > 1000
 
-    SERVE = Sum([L(r)*r.n for r in requests])
+CAPACITY = [Sum([If(has_video[i][j], S[i], 0) for i in range(V)]) <= X
+            for j in range(C)]
 
 
 def solve(constraints):
@@ -68,78 +67,10 @@ def solve(constraints):
     s.check()
     return s.model()
 
-
-
-
-print(solve(Constraints.CAPACITY))
+print(solve([CAPACITY, SERVE]))
 
 
 exit()
-
-R, C, L, H, rows = read_file('example.in')
-
-TOTAL_SLICES_SIZE = Int('TOTAL_SLICES_SIZE')
-
-
-def make_2darr(name, f):
-    ARR = Function(name, z3.IntSort(), z3.IntSort(), IntSort())
-    INIT = [ARR(r, c) == f(r, c) for r in range(R) for c in range(C)]
-    INIT += [ARR(r, -1) == 0 for r in range(-1, R)]
-    INIT += [ARR(-1, c) == 0 for c in range(-1, C)]
-
-    def call(s):
-        sr, sc = s.src.r, s.src.c
-        dr, dc = s.dst.r, s.dst.c
-        return ARR(dr, dc) - ARR(dr, sc - 1) - ARR(sr - 1, dc) + ARR(sr - 1, sc - 1)
-    return ARR, INIT, call
-
-
-total_mushrooms = np.cumsum(np.cumsum(rows, axis=0), axis=1)
-TOTAL, INIT_TOTAL, count_mushrooms = make_2darr('TOTAL', lambda r, c: int(total_mushrooms[r, c]))
-SIZES, INIT_SIZES, slice_size = make_2darr('SIZES', lambda r, c: (r + 1) * (c + 1))
-
-
-def VAR_SLICE_SIZE(i):
-    return Int('Slice{}.SIZE'.format(i))
-
-
-def VAR_MUSH_COUNT(i):
-    return Int('Slice{}.MUSH_COUNT'.format(i))
-
-
-def slice_constraints(s):
-    SLICE_SIZE = VAR_SLICE_SIZE(s.i)
-    MUSH_COUNT = VAR_MUSH_COUNT(s.i)
-    return And(  # slices are in bounds
-                s.src.r >= 0, s.src.c >= 0, s.dst.r < R, s.dst.c < C,
-                # slices are correctly shaped
-                s.src.r <= s.dst.r, s.src.c <= s.dst.c,
-                SLICE_SIZE == slice_size(s),
-                MUSH_COUNT == count_mushrooms(s),
-                L <= MUSH_COUNT,
-                L <= SLICE_SIZE - MUSH_COUNT,
-                # and at most H cells of any kind in total:
-                SLICE_SIZE <= H)
-
-
-def cons_nonoverlap(s1, s2):
-    # The slices being cut out cannot overlap
-    return Or(s1.dst.r < s2.src.r, s1.dst.c < s2.src.c)
-
-
-def create_slices(num):
-    slices = [SL(src=Point(c=Int("Slice{}.src.c".format(i)),
-                           r=Int("Slice{}.src.r".format(i))),
-                 dst=Point(c=Int("Slice{}.dst.c".format(i)),
-                           r=Int("Slice{}.dst.r".format(i))),
-                 i=i)
-              for i in range(num)]
-    constraints = [slice_constraints(s) for s in slices]
-    constraints += [cons_nonoverlap(s1, s2) for s1 in slices for s2 in slices if s1.i < s2.i]
-    constraints += [TOTAL_SLICES_SIZE == Sum([VAR_SLICE_SIZE(s.i) for s in slices]),
-                    TOTAL_SLICES_SIZE <= R * C]  # so we stop when we've found an obviously optimal solution
-    return slices, constraints
-
 
 def optimize(constraints, maximize=True):
     s = Optimize()
