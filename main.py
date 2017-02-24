2c19354465b803f70e6b03aa809e0036876c4a3a
elazar <elazarg@gmail.com>
2017-02-24 17:11:59 +0200

    structure

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 from collections import namedtuple, defaultdict, Counter
-from itertools import islice
 import z3
 
 Req = namedtuple('Req', ('v', 'e', 'n'))
 Endpoint = namedtuple('Endpoint', ('L_D', 'K', 'L'))
-
-BASENAME = 'trending_today'
-FILENAME = BASENAME + '.in'
-RES = {'me_at_the_zoo.in': 0,
-       'videos_worth_spreading.in': 0,
-       'kittens.in': 0,
-       'trending_today.in': 100000}
 
 
 def read_line_ints(line):
@@ -36,13 +28,18 @@ def read_file(filename):
     for line in txt[1 + 1 + sum(e.K + 1 for e in endpoints):]:
         v, e, n = read_line_ints(line)
         requests[(v, e)] += n
-    print('requests before:', len(requests))
-    requests = Counter(dict(requests.most_common(1000)))
-    print('requests after:', len(requests))
-    return (V, E, R, C, X), S, endpoints, requests
+    # print('requests before:', len(requests))
+    # requests = Counter(dict(requests.most_common(1000)))
+    # print('requests after:', len(requests))
+    relevant = defaultdict(set)
+    for (v, e) in requests:
+        for c, l in endpoints[e].L.items():
+            relevant[c].add(v)
+
+    return (V, E, R, C, X), S, endpoints, requests, relevant
 
 
-def make_dict(m):
+def make_dict(m, C, relevant):
     d = defaultdict(list)
     for j in range(C):
         for i in relevant[j]:
@@ -51,28 +48,13 @@ def make_dict(m):
     return d
 
 
-def write_file(d, result):
-    with open("{}/{}.txt".format(BASENAME, result), "w") as out:
+def write_file(basename, d, result):
+    with open("{}/{}.txt".format(basename, result), "w") as out:
         out.write(str(len(d)))
         out.write('\n')
         for k in d:
             out.write("{} {}".format(str(k), " ".join(map(str, d[k]))))
             out.write('\n')
-
-print('Reading file...', end=' ')
-(V, E, R, C, X), S, endpoints, requests = read_file(FILENAME)
-print('done.')
-
-relevant = defaultdict(set)
-for (v, e) in requests:
-    for c in endpoints[e].L:
-        relevant[c].add(v)
-
-print((V, E, R, C, X))
-print(S)
-print(endpoints)
-print(requests)
-print(relevant)
 
 
 def find_max(xs):
@@ -81,42 +63,40 @@ def find_max(xs):
         m = z3.If(m > x, m, x)
     return m
 
-
-def request_key(r):
-    return S[r.v] / r.n
-NUMREQ = 100
-
-print('Generate constraints...')
-
 has_video = z3.Function('has_video', z3.IntSort(), z3.IntSort(), z3.BoolSort())
-
 SERVE = z3.Int('SERVE')
-CAPVAR = [z3.Int('CAP_{}'.format(j)) for j in range(C)]
-
-print('SERVE_SUM')
-SERVE_SUM = SERVE == z3.Sum([find_max([z3.If(has_video(v, j), endpoints[e].L_D-endpoints[e].L[j], 0)
-                                       for j in endpoints[e].L]
-                                      )*n
-                             for (v, e), n in requests.items()])
-
-print('CAPVAR_SUM')
-CAPVAR_SUM = [CAPVAR[j] == z3.Sum([z3.If(has_video(i, j), S[i], 0) for i in relevant[j]]) for j in range(C)]
-print('CAPACITY')
-CAPACITY = [CAPVAR[j] <= X for j in range(C)]
-print('CAP_FULL')
-CAP_FULL = [z3.Or(has_video(i, j), CAPVAR[j] > X - S[i]) for j in range(C) for i in relevant[j]]
 
 
-def prepare_solver():
-    s = z3.Solver()
-    s.add(CAPVAR_SUM)
-    s.add(CAPACITY)
-    s.add(CAP_FULL)
-    s.add(SERVE_SUM)
+def generate_constraints(C, X, S, endpoints, requests, relevant):
+    print('Generate constraints...')
+
+    print('SERVE_SUM')
+    CAPVAR = [z3.Int('CAP_{}'.format(j)) for j in range(C)]
+    SERVE_SUM = SERVE == z3.Sum([find_max([z3.If(has_video(v, j), endpoints[e].L_D-endpoints[e].L[j], 0)
+                                           for j in endpoints[e].L]
+                                          )*n
+                                 for (v, e), n in requests.items()])
+
+    print('CAPVAR_SUM')
+    CAPVAR_SUM = [CAPVAR[j] == z3.Sum([z3.If(has_video(i, j), S[i], 0) for i in relevant[j]]) for j in range(C)]
+    print('CAPACITY')
+    CAPACITY = [CAPVAR[j] <= X for j in range(C)]
+    print('CAP_FULL')
+    CAP_FULL = [z3.Or(has_video(i, j), CAPVAR[j] > X - S[i])
+                for j in range(C) for i in relevant[j]]
+
+    return [CAPVAR_SUM, CAPACITY, CAP_FULL, SERVE_SUM]
+
+
+def prepare_solver(constraints, Solver=z3.Solver):
+    s = Solver()
+    for c in constraints:
+        s.add(c)
     return s
 
 
-def maximize(s):
+def maximize(constraints):
+    s = prepare_solver(constraints, z3.Optimize)
     s.maximize(SERVE)
     s.check()
     return s.model()
@@ -127,25 +107,41 @@ def solve(s, bound=0):
     s.check()
     try:
         return s.model()
-    except e:
+    except Exception:
         return None
 
 
-result = RES[FILENAME]
-print('preparing...')
-s = prepare_solver()
+def iterative(BASENAME):
+    FILENAME = BASENAME + '.in'
+    (_, _, _, C, X), S, endpoints, requests, relevant = read_file(FILENAME)
+    result = 0
+    print('preparing...')
+    s = prepare_solver(generate_constraints(C, X, S, endpoints, requests, relevant))
+    while True:
+        target = int(result + 1)
+        print('solving round for {}...'.format(target))
+        m = solve(s, target)
+
+        if not m:
+            print('UNSAT')
+            break
+        else:
+            result = int(str(m.evaluate(SERVE)))
+            print('found {}...'.format(result))
+            d = make_dict(m, C, relevant)
+            write_file(BASENAME, d, result)
 
 
-while True:
-    target = int(result*1.1)
-    print('solving round for {}...'.format(target))
-    m = solve(s, target)
+def optimize(BASENAME):
+    FILENAME = BASENAME + '.in'
+    (_, _, _, C, X), S, endpoints, requests, relevant = read_file(FILENAME)
+    print('preparing...')
+    m = maximize(generate_constraints(C, X, S, endpoints, requests, relevant))
+    result = int(str(m.evaluate(SERVE)))
+    print('found {}...'.format(result))
+    d = make_dict(m, C, relevant)
+    write_file(BASENAME, d, result)
 
-    if not m:
-        print('UNSAT')
-        break
-    else:
-        result = int(str(m.evaluate(SERVE)))
-        print('found {}...'.format(result))
-        d = make_dict(m)
-        write_file(d, result)
+
+optimize('me_at_the_zoo')
+iterative('trending_today')
