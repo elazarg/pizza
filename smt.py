08f37ad243e84c7cd6dc511c14d83f61de0efa2f
elazar <elazarg@gmail.com>
2017-02-26 17:47:15 +0200

    cpp

--- smt.py
+++ smt.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
-import z3
 import readfile as io
-
-
-def find_max(xs, v, ld):
-    m = 0
-    for c, latency in xs:
-        m = z3.If(z3.And(has_video(v, c), m < ld - latency), ld - latency, m)
-    return m
-
-has_video = z3.Function('has_video', z3.IntSort(), z3.IntSort(), z3.BoolSort())
-SERVE = z3.Int('SERVE')
+from collections import defaultdict
 
 
 def progressbar(xs):
@@ -24,11 +14,8 @@ def progressbar(xs):
 def generate_constraints(capacity, sizes, endpoints, caches):
     print('Generate constraints...')
 
-    print('SERVE_SUM')
-    yield SERVE == z3.Sum([find_max(e.L.items(), v, e.L_D) * ratio
-                           for e in progressbar(endpoints)
-                           for v, ratio in e.requests.most_common(100)])
-    for c, videos in caches.items():
+    d = defaultdict(list)
+    for c, videos in progressbar(list(caches.items())):
         def latency_gain(v):
             return sum(e.requests[v] * (e.L[c1] - e.L[c]) // sizes[v]
                        for e in endpoints
@@ -36,45 +23,23 @@ def generate_constraints(capacity, sizes, endpoints, caches):
                        if c in e.L and c1 != c)
         total = 0
         for v in sorted(videos, key=latency_gain, reverse=True):
-            if total + v == capacity:
+            s = sizes[v]
+            if total + s == capacity:
                 break
-            if total + v <= capacity:
-                total += v
-                yield has_video(v, c)
+            if total + s <= capacity:
+                total += s
+                d[c].append(v)
+    return d
 
 
-def iterative(basename, maximize=False):
+def greedy(basename, maximize=False, result=0):
     capacity, sizes, endpoints, caches = io.read_file(basename + '.in')
-    result = 29585985  # 25568559
     print('preparing...')
-    s = z3.Optimize() if maximize else z3.Solver()
-    for c in generate_constraints(capacity, sizes, endpoints, caches):
-        s.add(z3.simplify(c))
-    with open(basename + '.smt2', 'w') as f:
-        print(s.to_smt2(), file=f)
-    if maximize:
-        s.maximize(SERVE)
-    while True:
-        target = int(result)
-        print('solving round for', target)
-        s.add(SERVE > target)
-        s.check()
-        try:
-            m = s.model()
-        except z3.Z3Exception:
-            print('UNSAT')
-            break
-        result = int(str(m.evaluate(SERVE)))
-        print('found', result)
-        d = {c: [v for v in videos if m.evaluate(has_video(v, c))]
-             for c, videos in caches.items()}
-        io.write_file("{}/{}.txt".format(basename, result), d)
-        if maximize:
-            break
+    d = generate_constraints(capacity, sizes, endpoints, caches)
+    io.write_file("{}/{}.txt".format(basename, 'greedy'), d)
 
 if __name__ == '__main__':
-    iterative('me_at_the_zoo')
-    #iterative('kittens')
-    #iterative('videos_worth_spreading')
-
-    # iterative('trending_today')
+    greedy('trending_today')
+    greedy('kittens')
+    greedy('videos_worth_spreading')
+    greedy('me_at_the_zoo')
